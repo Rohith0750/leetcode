a7c176eaec90dad491cff45a6d3460ba93527e24
Rohith S D <rohithsd0222@gmail.com>
2026-03-03 10:06:04 +0530

    Time: 27 ms (25.21%), Space: 13.5 MB (31.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def isAnagram(self, s, t):
         return sorted(s.lower()) == sorted(t.lower())
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+      
         
