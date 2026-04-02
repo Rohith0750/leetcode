042ee979e1c1e1b504976e7eec861e78e5b2f9cc
Rohith S D <rohithsd0222@gmail.com>
2026-04-02 14:13:38 +0530

    Time: 1 ms (99.94%), Space: 46.7 MB (49.66%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+      if(magazine.length()<ransomNote.length()){
+        return false;
+      }  
+      int count[]=new int[26];
+      for(char c:magazine.toCharArray()){
+        count[c-'a']++;
+      }
+      for(char c:ransomNote.toCharArray()){
+        count[c-'a']--;
+        if(count[c-'a']<0){
+            return false;
+        }
+      }
+      return true;
+    }
+}
