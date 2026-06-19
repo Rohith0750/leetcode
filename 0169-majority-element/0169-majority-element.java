7f11c2da5b256405ecffdf441fc9a918b3ceaee2
Rohith S D <rohithsd0222@gmail.com>
2026-06-19 14:12:58 +0530

    Time: 1 ms (99.89%), Space: 55.8 MB (31.41%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,18 +2,39 @@ class Solution {
     public int majorityElement(int[] arr) {
         int n = arr.length;
 
-        HashMap<Integer,Integer>mpp=new HashMap<>();
-        for(int i=0;i<n;i++){
-            mpp.put(arr[i],mpp.getOrDefault(arr[i],0)+1);
-        }
+        // HashMap<Integer,Integer>mpp=new HashMap<>();
+        // for(int i=0;i<n;i++){
+        //     mpp.put(arr[i],mpp.getOrDefault(arr[i],0)+1);
+        // }
 
-        for(Map.Entry<Integer,Integer>ent:mpp.entrySet()){
-            if(ent.getValue()>n/2){
-                return ent.getKey();
+        // for(Map.Entry<Integer,Integer>ent:mpp.entrySet()){
+        //     if(ent.getValue()>n/2){
+        //         return ent.getKey();
+        //     }
+        // }
+        // return -1;
+        int cnt=0;
+        int ele=0;
+        for(int i=0;i<n;i++){
+            if(cnt==0){
+                cnt=1;
+                ele=arr[i];
+            }else if(arr[i] == ele){
+                cnt++;
+            }
+            else{
+                cnt--;
             }
         }
-        return -1;
-        
+        int cnt2=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==ele){
+                cnt2++;
+            }
+        }
+        if(cnt2>n/2){
+            return ele;
+        }
 
     // int n=arr.length;
     // for(int i=0;i<n;i++){
@@ -28,7 +49,7 @@ class Solution {
     //     }
         
     // }
-    // return -1;
+     return -1;
 
     }
 }
