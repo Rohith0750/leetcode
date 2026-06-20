6a6b9e5029b5781f0886dd864fb1ff18135c3ef5
Rohith S D <rohithsd0222@gmail.com>
2026-06-20 12:37:22 +0530

    Time: 4 ms (41.28%), Space: 175.5 MB (56.42%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        ArrayList<Integer> pos = new ArrayList<>();
-        ArrayList<Integer> neg = new ArrayList<>();
+        // ArrayList<Integer> pos = new ArrayList<>();
+        // ArrayList<Integer> neg = new ArrayList<>();
         // for(int i=0;i<nums.length;i++){
         //     if(nums[i]>0){
         //         pos.add(nums[i]);
@@ -13,27 +13,41 @@ class Solution {
         //         neg.add(nums[i]);
         //     }
         // }
+    // for(int i=0;i<nums.length;i++){
+    //     if(nums[i]>0){
+    //         pos.add(nums[i]);
+    //     }else
+    //     {
+    //         neg.add(nums[i]);
+    //     }
+    // }
+    //     int result[]=new int[nums.length];
+
+
+    //     int idx=0;
+    //     for(int i=0;i<pos.size();i++){
+    //         result[idx]=pos.get(i);
+    //         idx++;
+    //         result[idx]=neg.get(i);
+    //         idx++;
+    //     }
+
+
+    //     return result;
+
+    int pos=0;
+    int neg=1;
+    int ans[] = new int[nums.length];
     for(int i=0;i<nums.length;i++){
         if(nums[i]>0){
-            pos.add(nums[i]);
-        }else
-        {
-            neg.add(nums[i]);
+            ans[pos]=nums[i];
+            pos=pos+2;
+        }else{
+            ans[neg]=nums[i];
+            neg=neg+2;
         }
     }
-        int result[]=new int[nums.length];
-
-
-        int idx=0;
-        for(int i=0;i<pos.size();i++){
-            result[idx]=pos.get(i);
-            idx++;
-            result[idx]=neg.get(i);
-            idx++;
-        }
-
-
-        return result;
+    return ans;
  
     }
 }
