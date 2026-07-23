4a740fbc5279564ccd264893cef0267969c4a80c
Rohith S D <rohithsd0222@gmail.com>
2026-07-23 15:02:58 +0530

    Time: 1 ms (60.18%), Space: 48.8 MB (16.01%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,25 +1,26 @@
 class Solution {
     public int trap(int[] height) {
-        int n =height.length;
-        int leftsum[]= new int[n];
-         leftsum[0]=height[0];
-        for(int i=1;i<n;i++){
-            leftsum[i]=Math.max(height[i],leftsum[i-1]);
+
+    int n = height.length;
+    int leftmax[]=new int[n];
+    leftmax[0]=height[0];
+    for(int i=1;i<n;i++){
+        leftmax[i]=Math.max(leftmax[i-1],height[i]);
+    }
+
+    int rightmax[] = new int[n];
+    rightmax[n-1]=height[n-1];
+    for(int i=n-2;i>=0;i--){
+        rightmax[i]=Math.max(rightmax[i+1],height[i]);
+
+    }
+
+    int total=0;
+    for(int i=0;i<n;i++){
+        if(rightmax[i]>height[i] && leftmax[i]>height[i]){
+            total+=Math.min(leftmax[i],rightmax[i])-height[i];
         }
-
-
-        int rightmax[]=new int [n];
-        rightmax[n-1]=height[n-1];
-        for(int i=n-2;i>=0;i--){
-          rightmax[i]=Math.max(height[i],rightmax[i+1]);
-        }
-
-        int trappedwater=0;
-
-        for(int i=0;i<n;i++){
-            int waterlevel=Math.min(leftsum[i],rightmax[i]);
-            trappedwater += waterlevel-height[i] ;
-        }
-      return trappedwater;  
+    }
+    return total;
     }
 }
